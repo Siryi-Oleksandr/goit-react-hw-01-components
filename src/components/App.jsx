712d82f62a431f2b54d06a380cdeaafa0d7a06a5
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-02-15 14:10:08 +0200

    have done Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import user from '../user/user.json';
+import { TaskTitle } from './TaskTitle/TaskTitle';
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <TaskTitle text="1 - Профіль соціальної мережі" />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <TaskTitle text="2- Секція статистики" />
     </div>
   );
 };
